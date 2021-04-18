3e25ad84fbe6379fe12038037838a909692c1261
Josh <jtaustin19@icloud.com>
2021-04-17 20:54:30 -0500

    added comment on pizza option

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -1,31 +1,39 @@
 const { Pizza } = require('../models');
 
 const pizzaController = {
-    // get all pizzas
+    // GET all pizzas "/api/pizzas"
     getAllPizza(req, res) {
       Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
+          .populate({
+              path: "comments",
+              select: "-__v"
+          })
+          .select("-__v")
+          .sort({ _id: -1 })
+          .then(dbPizzaData => res.json(dbPizzaData))
+          .catch(err => {
+              console.log(err);
+              res.status(400).json(err);
+          });
+  },
   
-    // get one pizza by id
+    // GET one pizza "/api/pizzas/:id"
     getPizzaById({ params }, res) {
       Pizza.findOne({ _id: params.id })
-        .then(dbPizzaData => {
-          // If no pizza is found, send 404
-          if (!dbPizzaData) {
-            res.status(404).json({ message: 'No pizza found with this id!' });
-            return;
-          }
-          res.json(dbPizzaData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+          .populate({
+              path: "comments",
+              select: "-__v"
+          })
+          .select("-__v")
+          .then(dbPizzaData => {
+              if (!dbPizzaData) return res.status(404).json({ message: "No pizza found with this id!" });
+
+              res.json(dbPizzaData);
+          })
+          .catch(err => {
+              console.log(err);
+              res.status(400).json(err);
+          });
     },
 
     // createPizza
